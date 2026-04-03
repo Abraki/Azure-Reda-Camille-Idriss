703961cc73ae4a3bfbb66b768275c813d40bc886
Abraki <168692923+Abraki@users.noreply.github.com>
2026-04-03 17:26:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const path = require('path');
 
 // Configuration via les variables d'environnement (ce qu'on a configuré sur Azure)
-const config = {
-    user: process.env.User,
-    password: process.env.Password,
-    server: process.env.host,
-    database: process.env.Database,
+cconst config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_HOST,
+    database: process.env.DB_NAME,
     port: 1433,
     options: {
         encrypt: true,
