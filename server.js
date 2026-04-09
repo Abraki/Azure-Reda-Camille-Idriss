90228367b8824be49c38b89512e6240e8893999d
Abraki <168692923+Abraki@users.noreply.github.com>
2026-04-09 21:12:18 +0200

    Update server.js
    Fix typo in variable declaration for config.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 
 // Configuration via les variables d'environnement (ce qu'on a configuré sur Azure)
-cconst config = {
+const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_HOST,
