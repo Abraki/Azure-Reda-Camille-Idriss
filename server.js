82027beff9f4824ddbdae6a292fe6bc72380f88f
Sirdi93 <109238295+Sirdi93@users.noreply.github.com>
2026-04-10 12:41:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 // Configuration via les variables d'environnement (ce qu'on a configuré sur Azure)
 const config = {
     user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
   
     server: process.env.DB_HOST,
     database: process.env.DB_NAME,
